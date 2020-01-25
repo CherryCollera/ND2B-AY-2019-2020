6da5c2915445102968c0248fe9a060411df4dfe5
LouielAngeloQuisim <60286235+LouielAngeloQuisim@users.noreply.github.com>
2020-01-25 19:04:14 +0800

    Add files via upload

---
+++ LabActivity1_Quisim/ConsoleApp1/MyProfile.cs
@@ -0,0 +1,26 @@
+﻿/*18-0380
+ * Louiel Angelo C.Quisim
+ * BSCS-ND2B
+ * January 23, 2020
+ * This Program will display name, date of birth, course, year and section
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyProfile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:\t\t\t Louiel Angelo C Quisim");
+            Console.WriteLine("Date of Birth:\t\t August 3 1999");
+            Console.WriteLine("Course:\t\t\t BSCS");
+            Console.WriteLine("Year and Section:\t ND2B");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabActivity1_Quisim/Sample1_helloworld_Quisim/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿/*18-0380
+ * Louiel Angelo C.Quisim
+ * BSCS-ND2B
+ * January 23, 2020
+ * This Program will display Hello World
+ */
+//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1_helloworld_Quisim
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
