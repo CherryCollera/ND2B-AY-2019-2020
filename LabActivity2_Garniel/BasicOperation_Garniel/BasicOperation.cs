f8e732d7e69dd71c8ff92338ac60765473933f42
roselynnegarniel <60179493+roselynnegarniel@users.noreply.github.com>
2020-01-31 07:22:10 +0800

    Add files via upload

---
+++ LabActivity2_Garniel/BasicOperation_Garniel/BasicOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperation_Garniel
+{
+    class BasicOperation
+    {
+        static void Main(string[] args)
+        {
+            Double num1, num2;
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Sum = {0} ", num1 + num2);
+            Console.WriteLine("Difference = {0} ", num1 - num2);
+            Console.WriteLine("Product = {0} ", num1 * num2);
+            Console.WriteLine("Quotient = {0: 0.00} ", num1 / num2);
+            Console.WriteLine("Remainder = {0: 0.00} ", num1 % num2); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabActivity2_Garniel/DeclaringConstant_Garniel/DeclaringConstant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringConstant_Garniel
+{
+    class DeclaringConstant
+    {
+        static void Main(string[] args)
+       {
+
+            const double pi = 3.14159;
+            double radius;
+            Console.Write("Enter radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Radius:{0: 0.0000}, ", + radius);
+            Console.Write("Area:{0: 0.0000} ", (pi * radius * radius));
+            Console.ReadLine();
+        }
+    }
+}
