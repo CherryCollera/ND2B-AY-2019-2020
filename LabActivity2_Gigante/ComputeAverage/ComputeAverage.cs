ea5379ab344a4ea11bb665be9a92ad5b18f9ac61
dharielgigante <60182444+dharielgigante@users.noreply.github.com>
2020-01-31 17:36:04 +0800

    Add files via upload

---
+++ LabActivity2_Gigante/ComputeAverage/ComputeAverage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverage
+{
+    class ComputeAverage
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3, num4, num5;
+            Console.WriteLine("Enter 5 grades:  ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            num4 = Convert.ToInt32(Console.ReadLine());
+            num5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The average is = {0: 0.000}  ", (num1 + num2 + num3 + num4 + num5)/5);
+           
+            Console.ReadLine();
+        }
+    }
+}
