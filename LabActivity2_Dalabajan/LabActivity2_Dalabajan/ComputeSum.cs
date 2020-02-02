185f562c0e87470b6681e7bb3500d7a2489330ee
lesterjoi <60223505+lesterjoi@users.noreply.github.com>
2020-02-02 19:03:09 +0700

    Add files via upload

---
+++ LabActivity2_Dalabajan/LabActivity2_Dalabajan/ComputeSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabActivity2_Dalabajan
+{
+    class ComputeSum
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum = {0} ", num1 + num2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
