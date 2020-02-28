23c48216036bdba169f25f961eb296d9f911a926
LouielAngeloQuisim <60286235+LouielAngeloQuisim@users.noreply.github.com>
2020-02-28 14:28:56 +0800

    Add files via upload

---
+++ LabActivity4_DeGuzmanv2/BasicOperation_DeGuzman/Difference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperation_DeGuzman
+{
+    class Difference
+    {
+        public void ComputeDifference()
+        {
+            DeclareVar.difference = DeclareVar.num1 - DeclareVar.num2;
+        }
+    }
+}
---
+++ LabActivity4_DeGuzmanv2/BasicOperation_DeGuzman/Input.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperation_DeGuzman
+{
+    class Input
+    {
+        public void InputValues()
+        {
+            try
+            {
+                System.Console.Write("Enter first number: \t");
+                DeclareVar.num1 = System.Convert.ToInt32(System.Console.ReadLine());
+                System.Console.Write("Enter second number: \t");
+                DeclareVar.num2 = System.Convert.ToInt32(System.Console.ReadLine());
+            }
+            
+            catch(System.FormatException ex)
+            {
+                System.Console.Error.WriteLine("Error." + ex.Message);
+            }
+        }
+    }
+}
