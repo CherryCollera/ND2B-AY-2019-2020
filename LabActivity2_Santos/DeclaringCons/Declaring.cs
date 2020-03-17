c179dcce45e9f3dee3f11d9410fde4534c296a8e
kimsantos1234 <60533398+kimsantos1234@users.noreply.github.com>
2020-03-17 10:20:27 +0800

    Add files via upload

---
+++ LabActivity2_Santos/DeclaringCons/Declaring.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeclaringCons
+{
+    class Declaring
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            double radius;
+            Console.Write("Enter Radius:  ");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Radius: {0: 0.0000}", +radius);
+            Console.Write(",  Area: {0: 0.0000}  ", (pi * radius * radius));
+
+            Console.ReadLine();
+        }
+    }
+}
