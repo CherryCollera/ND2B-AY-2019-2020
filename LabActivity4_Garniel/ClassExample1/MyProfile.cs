a978e2d05942d959847994a1f59756927ee3dfe0
roselynne garniel <60179493+roselynnegarniel@users.noreply.github.com>
2020-03-17 10:05:23 +0800

    Add files via upload

---
+++ LabActivity4_Garniel/ClassExample1/MyProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample1
+{
+    class MyProfile
+    {
+        public void DisplayProfile()
+        {
+            System.Console.WriteLine("\n\n\n\t\t\tP R O F I L E\n");
+            System.Console.WriteLine("Name\t:\t\tRoselynne Joisse D. Garniel\n");
+            System.Console.WriteLine("Birtday\t:\t\tMarch 22, 2000\n");
+            System.Console.WriteLine("Couse\t:\t\tBS Computer Science Major in Network" + " and Data Communication\n");
+            System.Console.WriteLine("Year\t:\t\t2nd Year\n");
+            System.Console.WriteLine("Section\t:\t\tB\n");
+            System.Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ LabActivity4_Garniel/ClassExample1/Print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample1
+{
+    class Print
+    {
+        public void PrintDetails()
+        {
+            Accept a = new Accept();
+            a.AcceptDetails();
+            System.Console.Write("Hello " + a.firstname + " " + a.lastname + "!!!\nYou have Created Classes in OOP");
+            MyProfile mp = new MyProfile();
+            mp.DisplayProfile();
+
+        }
+    }
+}
+
